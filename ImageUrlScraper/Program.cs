8285aaa0e8887771b4feadb09a945d87b19d507e
Apurva Kaluskar <kaluskap@SYNOVERGETECH.COM>
2019-02-15 13:15:40 +0530

    First commit

---
+++ ImageUrlScraper/Program.cs
@@ -0,0 +1,72 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ImageUrlScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> templateList = new List<string>();
+            string connection = "<connstring>";
+            string query = "<query>";
+            string path = @"filepath";
+
+            using (OracleConnection con = new OracleConnection(connection))
+            {
+                using (OracleCommand cmd = con.CreateCommand())
+                {
+                    con.Open();
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = query;
+                    OracleDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string template = Convert.ToString(reader.GetValue(reader.GetOrdinal("VALUE")));
+                        templateList.Add(template);
+                    }
+                }
+            }
+            
+            if (!File.Exists(path))
+                File.Create(path);
+
+            TextWriter tw = new StreamWriter(path);
+            List<Uri> imageUrlList = ScrapeImageUrls(templateList);
+            foreach (Uri imageUri in imageUrlList)
+                tw.WriteLine(Convert.ToString(imageUri));
+
+            WriteLine("Done");
+            tw.Close();
+
+            ReadLine();
+        }
+
+        public static List<Uri> ScrapeImageUrls(List<string> templateList)
+        {
+            List<Uri> links = new List<Uri>();
+            string regexImgSrc = @"<img[^>]*?src\s*=\s*[""']?([^'"" >]+?)[ '""][^>]*?>";
+
+            foreach (string htmlTemplate in templateList)
+            {
+                MatchCollection matchesImgSrc = Regex.Matches(htmlTemplate, regexImgSrc, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+                foreach (Match m in matchesImgSrc)
+                {
+                    string href = m.Groups[1].Value;
+                    links.Add(new Uri(href));
+                }
+            }
+
+            return links;
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Today I was allocated a task to go through 30 html templates and fetch the URL's of images in each template. These were email templates stored in Oracle DB.
+
+After doing it manually for 3 templates, I was bored.
+
+So this project is basically my brain having fun at work.
